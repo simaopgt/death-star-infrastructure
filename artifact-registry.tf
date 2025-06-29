db900d98281aace74914294b709e1c75edaf78cc
simaopgt <simaopgt@gmail.com>
2025-06-29 11:35:45 -0300

    Remove remote repository configuration, keep only standard repository

--- artifact-registry.tf
+++ artifact-registry.tf
@@ -1,19 +1,3 @@
-resource "google_artifact_registry_repository" "remote_repo" {
-  provider      = google-beta
-  project       = var.project_id
-  location      = var.region
-  repository_id = var.artifact_registry_repo_name
-  description   = "Remote repository for ghcr.io"
-  format        = "DOCKER"
-  mode          = "REMOTE_REPOSITORY"
-  remote_repository_config {
-    docker_repository {
-      custom_repository {
-        uri = "https://ghcr.io"
-      }
-    }
-  }
-}
 
 resource "google_artifact_registry_repository" "standard_repo" {
   project       = var.project_id
